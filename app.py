b5b54b960c8e1c1e1006a231aa30e5af3d035453
saqib-shigri <saqibhshigri@gmail.com>
2026-05-04 16:17:07 +0500

    fixed port binding for railways

--- app.py
+++ app.py
@@ -454,4 +454,6 @@ def admin_delete_job(id):
     return redirect(url_for('admin_jobs'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
