c96a036ce3e9efb97afff41119c64c41d445e602
saqib-shigri <saqibhshigri@gmail.com>
2026-05-04 11:54:46 +0500

    fixed verel flask

--- api/index.py
+++ api/index.py
@@ -6,5 +6,5 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
 
-# Vercel serverless function handler
+# This is for Vercel serverless function
 handler = app
